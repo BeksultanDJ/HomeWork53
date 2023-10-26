ad19b33b19b5445c20fc34f18b6b9af29a1e83c8
Beksultan Dzhienbekov <bekadj14@gmail.com>
2023-10-26 17:44:12 +0600

    Слепил все воедино чтобы все отображалось на странице

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
-
-import './App.css'
+import React, { useState } from 'react';
+import Task from './Task';
+import AddTaskForm from './AddTaskForm';
+import './App.css';
 
 function App() {
+    const [tasks, setTasks] = useState([
+        { id: '1', text: 'Fix a sink' },
+        { id: '2', text: 'Pay fo bills' },
+    ]);
 
+    const addTask = (text) => {
+        const newTask = {
+            id: new Date().getTime().toString(),
+            text,
+        };
+        setTasks([...tasks, newTask]);
+    };
 
-  return (
-      <></>
-  )
+    const deleteTask = (taskId) => {
+        const updatedTasks = tasks.filter((task) => task.id !== taskId);
+        setTasks(updatedTasks);
+    };
+
+    return (
+        <div className="app-box">
+            <AddTaskForm onAdd={addTask} />
+            <div className="tasks-box">
+                {tasks.map((task) => (
+                    <Task key={task.id} task={task} onDelete={deleteTask} />
+                ))}
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
